f2fdbdb842b7f167577ed4e163b25a873dd865bb
emil3566 <emil3566@gmail.com>
2014-07-30 17:37:49 +0200

    smallGraph incoming :)

--- src/SmallGraph/SmallGraph.mli
+++ src/SmallGraph/SmallGraph.mli
@@ -1,27 +1,27 @@
-module type SmallGraph = sig
+module type smallGraph = sig
 
 (* Type of data from vertices. 
 If someone want put data in vertices it should be abstract so people can put everything in it. *)
-type Vertex_t
+type vertex_t
 
 
 (* Function to compare vertices, since they can have abstract types we need compare funtion.
    use : compare a b > 0    if   a > b 
          compare a b = 0    if   a = b
          compare a b < 0    if   a < b *)
-val Vertex_compare : Vertex_t -> Vertex_t -> int 
+val vertex_compare : vertex_t -> vertex_t -> int 
 
 
 (* Type of data from edges. 
 If someone want put data in edges it should be abstract so people can put everything in it. 
 Like weights in Dijkstra or something like that.*)
-type Edge_t 
+type edge_t 
 
 
 (* Similar funtion to compare as it is in Vertex_compare, look higher *)
-val Edge_compare : Edge_t -> Edge_t -> int
+val edge_compare : edge_t -> edge_t -> int
 
-(* Array of array of Edge_t, that's how we will represent SmallGraph *)
-val Connetions : Edge_t array array
+(* funtion which create our graph *)
+val create : int -> edge_t array array
 
 end
